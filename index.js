dd4ea9f2d92bd1a088fc7f316fd68815a09b58fc
Karan Roushan <karan.122roushan@gmail.com>
2024-03-13 03:54:37 +0530

    created an object

---
+++ index.js
@@ -0,0 +1,13 @@
+
+// objects
+
+const profile = {
+  username: "Karan Roushan",
+  nickname:null,
+  posts: 0,
+  followers: 97,
+  following: 278,
+  profession: "full stack developer"
+};
+
+console.log(profile);
