531b73e43ba83a2bf3a28432f6080b3e2b985c58
Karan Roushan <karan.122roushan@gmail.com>
2024-03-13 04:31:10 +0530

    learnt about == and === operator

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
-
 // objects
 
 const profile = {
   username: "Karan Roushan",
-  nickname:null,
+  nickname: null,
   posts: 0,
   followers: 97,
   following: 278,
-  profession: "full stack developer"
+  profession: "full stack developer",
 };
 
-console.log(profile);
+console.log(typeof profile["profession"]);
+
+//difference between == & ===
+
+const a = 9;
+const b = "9";
+
+console.log("a == b", a == b); //true because it only checks values inside,
+console.log("a === b", a === b); //it is more strict, false because it check even datatype
+
